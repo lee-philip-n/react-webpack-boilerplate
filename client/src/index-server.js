c790682c634405f4fe37448c0cd82bd0e1c5e54d
lee-philip-n <lee.philip.n@gmail.com>
2018-03-31 19:19:36 -0700

    react-webpack-server

--- client/src/index-server.js
+++ client/src/index-server.js
@@ -1,3 +1,3 @@
 import App from './App';
 
-window.App = App;
+export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,54 @@
+const webpack = require('webpack');
 const path = require('path');
 
-module.exports = {
-  entry: path.resolve(__dirname, 'client/src/index.jsx'),
+const client = {
+  context: path.resolve(__dirname, 'client'),
+  entry: path.resolve(__dirname, './client/src/index.jsx'),
   output: {
     path: path.resolve(__dirname, 'client/dist'),
     filename: 'bundle.js',
   },
   module: {
     rules: [
-      { test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel-loader' },
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        loader: ['style-loader', 'css-loader'],
+      },
     ],
   },
   resolve: { extensions: ['.js', '.jsx'] },
 };
+
+const server = {
+  context: path.resolve(__dirname, 'client'),
+  entry: path.resolve(__dirname, 'client/src/index-server.js'),
+  target: 'node',
+  output: {
+    path: path.resolve(__dirname, 'client/dist'),
+    filename: 'bundle-server.js',
+    libraryTarget: 'commonjs',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        loader: 'css-loader',
+      },
+    ],
+  },
+  resolve: { extensions: ['.js', '.jsx'] },
+};
+
+module.exports = [
+  client, server,
+];
